a08917b3d9ac3f4600ca8395e691dd715bde8a7f
shahm32 <shahm32@mymacewan.ca>
2024-03-13 02:05:06 +0800

    Add project files.

---
+++ CSharp-Assignment1/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using CSharp_Assignment1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var student1 = Student.CreateStudent("1", StudentType.FullTime, "John", "Anderson");
+        var student2 = Student.CreateStudent("2", StudentType.FullTime, "Alice", "Peters");
+        var student3 = Student.CreateStudent("3", StudentType.PartTime, "Karen", "Smith");
+
+        Console.WriteLine("Assignment One:");
+        Student.OutputStudent(student1);
+        Student.OutputStudent(student2);
+        Student.OutputStudent(student3);
+    }
+}
+
---
+++ CSharp-Assignment1/Student.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Assignment1
+{
+    
+    public class Student
+    {
+        public int Id { get; set; }
+        public StudentType Type { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public static Student CreateStudent(string id, StudentType type, string firstName, string lastName)
+        {
+            if (string.IsNullOrEmpty(id))
+                id = "0";
+
+            if (firstName == null)
+                firstName = "Unknown";
+
+            if (lastName == null)
+                lastName = "";
+
+            return new Student
+            {
+                Id = int.Parse(id),
+                Type = type,
+                FirstName = firstName,
+                LastName = lastName
+            };
+        }
+
+        public static void OutputStudent(Student student)
+        {
+            Console.WriteLine($"Student: {student.Id:D3} {student.FirstName} {student.LastName} ({student.Type})");
+        }
+    }
+
+}
